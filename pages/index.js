6dcdae8755394f1ed75ecd7562fde13986c0ae1a
andres alcocer <devandres.tech@gmail.com>
2022-03-09 11:08:35 -0500

    set playsinline prop to react player

--- pages/index.js
+++ pages/index.js
@@ -37,6 +37,7 @@ export default function Home({}) {
             loop={true}
             width='100%'
             height='100%'
+            playsinline
             volume={1}
             muted={isMuted}
             controls={false}
