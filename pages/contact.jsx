1a96e6af009ae8e2fc83aeeb630748605943f2a4
andres alcocer <devandres.tech@gmail.com>
2022-03-10 14:36:38 -0500

    set layout styles

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,7 +1,11 @@
 import Head from 'next/head'
+import { useTranslations } from 'next-intl'
+
+import styles from '../styles/contact.module.scss'
 import Layout from '../components/layout/layout'
 
 export async function getStaticProps({ locale }) {
+  console.log('locale', locale)
   return {
     props: {
       messages: await (await import('../messages/es.json')).default,
@@ -10,6 +14,8 @@ export async function getStaticProps({ locale }) {
 }
 
 export default function Contact() {
+  const t = useTranslations('Contact')
+
   return (
     <Layout>
       <Head>
@@ -18,7 +24,19 @@ export default function Contact() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <main>
-        <h1>contact</h1>
+        <div className={styles.container}>
+          <div className={styles.left}>
+            <h2>{t('Info')}</h2>
+            <div className={styles.info}>
+              <div className={styles.infoItem}>
+                <i className={`fa-solid fa-envelope ${styles.icon}`}></i>
+              </div>
+            </div>
+          </div>
+          <div className={styles.right}>
+            <h2>{t('Questions')}</h2>
+          </div>
+        </div>
       </main>
     </Layout>
   )
