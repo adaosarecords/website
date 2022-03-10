d8e77e4117bf3374e0ea80f7eb6380dd321899b1
andres alcocer <devandres.tech@gmail.com>
2022-03-10 18:47:48 -0500

    set contact form validation

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -19,9 +19,32 @@ export default function Contact() {
   const [name, setName] = React.useState('')
   const [email, setEmail] = React.useState('')
   const [message, setMessage] = React.useState('')
-  const [nameErrorMessage, setNameErrorMessage] = React.useState('')
+  const [nameError, setNameError] = React.useState('')
+  const [emailError, setEmailError] = React.useState('')
+  const [messageError, setMessageError] = React.useState('')
+  const [isFormValid, setIsFormValid] = React.useState(false)
+
+  React.useEffect(() => {
+    if (
+      nameError.length > 0 ||
+      emailError.length > 0 ||
+      messageError.length > 0 ||
+      name.length <= 0 ||
+      email.length <= 0 ||
+      message.length <= 0
+    ) {
+      setIsFormValid(false)
+    } else {
+      setIsFormValid(true)
+    }
+  }, [messageError, nameError, emailError, isFormValid, name, email, message])
+
+  React.useEffect(() => {
+    setIsFormValid(false)
+  }, [])
 
   const submitForm = (e) => {
+    console.log('submitedd')
     e.preventDefault()
   }
 
@@ -63,44 +86,68 @@ export default function Contact() {
                 <label htmlFor='name'>{t('Name')}</label>
                 <input
                   style={{
-                    border: nameErrorMessage.length > 0 ? '3px solid red' : '',
+                    border: nameError.length > 0 ? '3px solid red' : '',
                   }}
                   value={name}
                   onChange={(e) => {
                     setName(e.target.value)
                     e.target.value.length > 0
-                      ? setNameErrorMessage('')
-                      : setNameErrorMessage('El campo no puede estar vacio')
+                      ? setNameError('')
+                      : setNameError('El campo no puede estar vacio')
                   }}
                   placeholder='mi nombre'
                   type='text'
                 />
-                {nameErrorMessage.length > 0 && (
-                  <span className={styles.errorMessage}>
-                    {nameErrorMessage}
-                  </span>
+                {nameError.length > 0 && (
+                  <span className={styles.errorMessage}>{nameError}</span>
                 )}
               </div>
               <div className={styles.formItem}>
                 <label htmlFor='email'>{t('Mail')}</label>
                 <input
+                  style={{
+                    border: emailError.length > 0 ? '3px solid red' : '',
+                  }}
                   value={email}
-                  onChange={(e) => setEmail(e.target.value)}
+                  onChange={(e) => {
+                    setEmail(e.target.value)
+                    e.target.value
+                      .trim()
+                      .match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i)
+                      ? setEmailError('')
+                      : setEmailError('Email inválido')
+                  }}
                   placeholder='micorreo@ejemplo.com'
                   type='text'
                 />
+                {emailError.length > 0 && (
+                  <span className={styles.errorMessage}>{emailError}</span>
+                )}
               </div>
-              <div className={styles.formItem}>
+              <div className={styles.formTextArea}>
                 <label htmlFor='text'>{t('Message')}</label>
                 <textarea
+                  style={{
+                    border: messageError.length > 0 ? '3px solid red' : '',
+                  }}
                   value={message}
-                  onChange={(e) => setMessage(e.target.value)}
+                  onChange={(e) => {
+                    setMessage(e.target.value)
+                    e.target.value.length > 0
+                      ? setMessageError('')
+                      : setMessageError('El campo no puede estar vacio')
+                  }}
                   placeholder='mi mensaje'
-                  rows={10}
+                  rows={5}
                 />
+                {messageError.length > 0 && (
+                  <span className={styles.errorMessage}>{messageError}</span>
+                )}
               </div>
               <div className={styles.formBtn}>
-                <button onClick={submitForm}>{t('Send')}</button>
+                <button disabled={!isFormValid} onClick={submitForm}>
+                  {t('Send')}
+                </button>
               </div>
             </form>
           </div>
