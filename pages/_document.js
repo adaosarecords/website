617e1b96dbefdd7fe4a440959cb8aad4ae9e1cf6
andres alcocer <devandres.tech@gmail.com>
2022-03-13 18:27:00 -0400

    set fav icon

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,7 @@ export default function Document() {
           href='https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,700;1,400;1,700&family=Nothing+You+Could+Do&family=Oswald:wght@200;400;700&display=swap'
           rel='stylesheet'
         />
+        <link rel='icon' href='/images/logo-orange.png' />
       </Head>
       <body>
         <Main />
