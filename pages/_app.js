a7198ff79aa95e4426aa66a2c41b9084c00a576c
andres alcocer <devandres.tech@gmail.com>
2022-03-16 16:21:17 -0400

    set up google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
-import '../styles/globals.scss'
+import React from 'react'
+import { useRouter } from 'next/router'
 import Script from 'next/script'
 import { NextIntlProvider } from 'next-intl'
 import SwiperCore, {
@@ -13,8 +14,29 @@ import 'swiper/css/navigation'
 import 'swiper/css/pagination'
 import 'swiper/css/scrollbar' // Pagination module
 
+import '../styles/globals.scss'
+import * as ga from '../lib/ga'
+
 function MyApp({ Component, pageProps }) {
   SwiperCore.use([Autoplay, Navigation, Pagination])
+  const router = useRouter()
+
+  React.useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url)
+    }
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange)
+    router.events.on('routeChangeError', handleRouteChange)
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+      router.events.off('routeChangeError', handleRouteChange)
+    }
+  }, [router.events])
 
   return (
     <NextIntlProvider locale='es' messages={pageProps.messages}>
--- pages/_document.js
+++ pages/_document.js
@@ -26,11 +26,28 @@ export default function Document() {
           `,
           }}
         />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-WQ8BNLB');`,
+          }}
+        />
+
         <link rel='icon' href='/images/logo-orange.png' />
         <link rel='alternate' href='http://example.com' hrefLang='en-us' />
         <link rel='alternate' href='http://example.com/es' hrefLang='es-mx' />
       </Head>
       <body>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WQ8BNLB"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        ></noscript>
+
         <Main />
         <NextScript />
       </body>
