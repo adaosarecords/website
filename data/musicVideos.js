ff9cdc441066b0a54993ee1bbe6fef36783c8044
andres alcocer <devandres.tech@gmail.com>
2022-03-17 19:50:13 -0400

    updated latest videos

--- data/musicVideos.js
+++ data/musicVideos.js
@@ -12,6 +12,48 @@ export const MUSIC_VIDEOS = [
     imageUrl: '/images/thumbvideos/raza.png',
   },
   {
+    id: 7,
+    title: 'Ritmo J y B-Que Dolor || En Vivo 2022',
+    videoUrl: 'https://www.youtube.com/watch?v=Semtok4p-Jk',
+    imageUrl: '/images/thumbvideos/jb.png',
+  },
+  {
+    id: 8,
+    title: 'Los Kochos De Guerrero Ft. Los Donnys De Guerrero',
+    videoUrl: 'https://www.youtube.com/watch?v=JnM4Gmdro6g',
+    imageUrl: '/images/thumbvideos/kochos.png',
+  },
+  {
+    id: 9,
+    title: 'Latinos De La Costa || Cumbias',
+    videoUrl: 'https://www.youtube.com/watch?v=sa0GBjaWjJ0',
+    imageUrl: '/images/thumbvideos/latinos.png',
+  },
+  {
+    id: 10,
+    title: 'Los Teclitas Jr. Popurri El Periquito || En vivo',
+    videoUrl: 'https://www.youtube.com/watch?v=sa0GBjaWjJ0',
+    imageUrl: '/images/thumbvideos/jr.png',
+  },
+  {
+    id: 11,
+    title: 'Los Donnys En Tlaxcaliztlahuaca, Guerrero',
+    videoUrl: 'https://www.youtube.com/watch?v=IGBpiE1oQeI',
+    imageUrl: '/images/thumbvideos/donnys2.png',
+  },
+  {
+    id: 12,
+    title: 'Raza Costeña - Revienta Cadera || En Vivo',
+    videoUrl: 'https://www.youtube.com/watch?v=533rgC2Qyjg',
+    imageUrl: '/images/thumbvideos/raza2.png',
+  },
+  {
+    id: 13,
+    title: 'Asi Se Baila En La Costa',
+    videoUrl: 'https://www.youtube.com/watch?v=kyHo6bZIGNo',
+    imageUrl: '/images/thumbvideos/costa.png',
+  },
+  {
     id: 3,
     title: 'Los Karkiks || Popurri Acapulco Tropical',
     videoUrl: 'https://www.youtube.com/watch?v=0cX2IM7LuzA',
